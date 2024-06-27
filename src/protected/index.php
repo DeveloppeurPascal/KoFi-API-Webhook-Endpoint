bc823f479b6ff225bd1de6a42a1740f2a50c2f72
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-06-27 18:16:39 +0200

    added default redirection to parent folder

---
+++ src/protected/index.php
@@ -0,0 +1,17 @@
+<?php
+	// Ko-fi API webhook endpoint
+	// (c) Patrick Prémartin
+	//
+	// Distributed under license AGPL-3.0 license.
+	//
+	// Infos and updates :
+	// https://github.com/DeveloppeurPascal/KoFi-API-Webhook-Endpoint
+
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// !!! NEVER CHANGE THIS FILE ON YOUR SITES !!!
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	// This file will be overwritten each time you update your site from the code 
+	// repository. Never modify it directly.
+	
+	header("location: ../");
